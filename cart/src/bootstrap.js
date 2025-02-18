4adceff70a516c2595dada7dcbd4d49f939b7737
Shail Shah Personal <shailshah.officework@gmail.com>
2025-02-18 15:40:24 +0530

    sharing dependencies between apps

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,31 @@
 import faker from "faker";
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.querySelector("#cart-dev").innerHTML = cartText;
+  el.innerHTML = cartText;
+};
+
+// Context/Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file
+// Which DEFNITELY has an element with an id of 'dev-cart'
+// We want to immediately render our app into that element
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#cart-dev");
+
+  // Assuming our container doesn't have an element
+  // with id 'cart-dev'...
+
+  if (el) {
+    // We are probably running in isolation
+    mount(el);
+  }
+}
+
+// Context/Situation #2
+// We are running this file in develpment or production
+// through the CONTAINER app
+// NO GUARANTEE that an element with an id of 'dev-cart' exists
+// WE DO NOT WANT try to immediately render the app
+export { mount };
